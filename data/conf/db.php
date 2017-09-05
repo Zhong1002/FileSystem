c4aec2887cec9724a67070ccb25276d2dc5bdd98
Zhong <zhong.jianquan@foxmail.com>
2017-09-05 13:14:18 +0800

    1

--- data/conf/db.php
+++ data/conf/db.php
@@ -21,8 +21,8 @@ return array(
 		'DB_TYPE' => 'mysql',
 		'DB_HOST' => 'localhost',
 		'DB_NAME' => 'filesystem',
-		'DB_USER' => 'root',
-		'DB_PWD' => '',
+		'DB_USER' => 'filesystem',
+		'DB_PWD' => 'filesystem',
 		'DB_PORT' => '3306',
 		'DB_PREFIX' => 'sp_',
 		//密钥
