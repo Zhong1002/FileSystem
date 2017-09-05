d1c588acd7dd4509f2dbeea669ecbc7a4530125f
Zhong <zhong.jianquan@foxmail.com>
2017-09-05 11:41:25 +0800

    1

--- data/conf/db.php
+++ data/conf/db.php
@@ -22,7 +22,7 @@ return array(
 		'DB_HOST' => 'localhost',
 		'DB_NAME' => 'filesystem',
 		'DB_USER' => 'root',
-		'DB_PWD' => 'l3201234',
+		'DB_PWD' => '',
 		'DB_PORT' => '3306',
 		'DB_PREFIX' => 'sp_',
 		//密钥
